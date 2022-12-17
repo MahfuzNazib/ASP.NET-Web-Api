682c76db8a899f8035a070bd4ecc1064d7dd6e11
MahfuzNazib <nazibmahfuz60@gmail.com>
2022-12-17 18:33:03 +0600

    first commit

---
+++ WebApiDemo/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        // GET: api/Demo
+        [HttpGet]
+        public string GetInfo()
+        {
+            return "Hello Demo API";
+        }
+
+        // GET : api/Demo/5
+        [HttpGet("{id}")]
+        public string UserInfo()
+        {
+            return "User Info with Perameter";
+        }
+    }
+}
---
+++ WebApiDemo/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/Users
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] 
+            { 
+                "Nazib",
+                "Mahfuz",
+                "Nadia",
+                "Nadrin",
+                "Bithi",
+                "Noorani",
+                "Noirita"
+            };
+        }
+
+        // GET api/Users/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/Users
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/Users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/Users/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        // GET api/Users/UpdateKey/5
+        [HttpGet("{userId}")]
+        public string UpdateKey(int userId)
+        {
+            return "This is Update Key";
+        }
+    }
+}
