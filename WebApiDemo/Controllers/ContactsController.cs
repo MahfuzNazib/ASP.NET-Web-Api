c4acbccc56566f6f49f2b0c59595bbadf346c357
MahfuzNazib <nazibmahfuz60@gmail.com>
2022-12-18 18:04:56 +0600

    Add New Schema Called Patient. Connect to DB. Get All PatientList from DB. Use ASP.NET Core Entity Framework

--- WebApiDemo/Controllers/ContactsController.cs
+++ WebApiDemo/Controllers/ContactsController.cs
@@ -5,7 +5,7 @@ using WebApiDemo.Models;
 
 namespace WebApiDemo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
 
     public class ContactsController : ControllerBase
